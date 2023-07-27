fe44d7b254cedca93fc3c439eaebdb1410223ce4
janus-zheng <lilei.zheng@tokopedia.com>
2023-07-27 06:53:18 +0000

    train on deepfashion data

--- train_deepfashion_mm.py
+++ train_deepfashion_mm.py
@@ -5,5 +5,5 @@ from ultralytics import YOLO
 model = YOLO('yolov8n-cls.pt')  # load a pretrained model (recommended for training)
 
 # Train the model
-model.train(data='/home/jupyter/data/color_dataset/digikala_9k/',
-            epochs=100, imgsz=320)
+model.train(data='/home/jupyter/data/color_dataset/deepfashion_mixed_tokopedia',
+            epochs=100, imgsz=160)
